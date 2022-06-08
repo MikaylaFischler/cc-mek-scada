ac607f9dc6da08c82bc59e7b0a706983f949686d
Mikayla Fischler <mikayla@ky8.io>
2022-06-08 16:21:49 -0400

    #63 latching button in addition to pushbutton

---
+++ graphics/elements/button_push.lua
@@ -0,0 +1,58 @@
+-- Button Graphics Element
+
+local tcd     = require("scada-common.tcallbackdsp")
+
+local element = require("graphics.element")
+
+---@class push_button_args
+---@field text string button text
+---@field callback function function to call on touch
+---@field min_width? integer text length + 2 if omitted
+---@field active_fg_bg? cpair foreground/background colors when pressed
+---@field parent graphics_element
+---@field x? integer 1 if omitted
+---@field y? integer 1 if omitted
+---@field height? integer parent height if omitted
+---@field gframe? graphics_frame frame instead of x/y/width/height
+---@field fg_bg cpair foreground/background colors
+
+-- new push button
+---@param args push_button_args
+local function push_button(args)
+    local text_width = string.len(args.text)
+    args.width = math.max(text_width + 2, args.min_width)
+
+    -- create new graphics element base object
+    local e = element.new(args)
+
+    local h_pad = math.floor((e.frame.w - text_width) / 2)
+    local v_pad = math.floor(e.frame.h / 2) + 1
+
+    -- write the button text
+    e.window.setCursorPos(h_pad, v_pad)
+    e.write(args.text)
+
+    -- handle touch
+    function e.handle_touch(event)
+        if args.active_fg_bg ~= nil then
+            -- show as pressed
+            e.window.setTextColor(args.active_fg_bg.fgd)
+            e.window.setBackgroundColor(args.active_fg_bg.bkg)
+            e.window.redraw()
+
+            -- show as unpressed in 0.5 seconds
+            tcd.dispatch(0.5, function ()
+                e.window.setTextColor(e.fg_bg.fgd)
+                e.window.setBackgroundColor(e.fg_bg.bkg)
+                e.window.redraw()
+            end)
+        end
+
+        -- call the touch callback
+        args.callback()
+    end
+
+    return e.get()
+end
+
+return push_button
--- graphics/elements/button_switch.lua
+++ graphics/elements/button_switch.lua
@@ -1,14 +1,13 @@
 -- Button Graphics Element
 
-local tcd     = require("scada-common.tcallbackdsp")
-
 local element = require("graphics.element")
 
----@class button_args
+---@class switch_button_args
 ---@field text string button text
 ---@field callback function function to call on touch
+---@field default? boolean default state, defaults to off (false)
 ---@field min_width? integer text length + 2 if omitted
----@field active_fg_bg? cpair foreground/background colors when pressed
+---@field active_fg_bg cpair foreground/background colors when pressed
 ---@field parent graphics_element
 ---@field x? integer 1 if omitted
 ---@field y? integer 1 if omitted
@@ -16,9 +15,13 @@ local element = require("graphics.element")
 ---@field gframe? graphics_frame frame instead of x/y/width/height
 ---@field fg_bg cpair foreground/background colors
 
--- new button
----@param args button_args
-local function button(args)
+-- new switch button (latch high/low)
+---@param args switch_button_args
+local function switch_button(args)
+    -- button state (convert nil to false if missing)
+    local state = args.default or false
+
+    -- determine widths
     local text_width = string.len(args.text)
     args.width = math.max(text_width + 2, args.min_width)
 
@@ -32,27 +35,35 @@ local function button(args)
     e.window.setCursorPos(h_pad, v_pad)
     e.write(args.text)
 
-    -- handle touch
-    function e.handle_touch(event)
-        if args.active_fg_bg ~= nil then
+    -- show the button state
+    local function draw_state()
+        if state then
             -- show as pressed
             e.window.setTextColor(args.active_fg_bg.fgd)
             e.window.setBackgroundColor(args.active_fg_bg.bkg)
-            e.window.redraw()
-
-            -- show as unpressed in 0.5 seconds
-            tcd.dispatch(0.5, function ()
-                e.window.setTextColor(e.fg_bg.fgd)
-                e.window.setBackgroundColor(e.fg_bg.bkg)
-                e.window.redraw()
-            end)
+        else
+            -- show as unpressed
+            e.window.setTextColor(e.fg_bg.fgd)
+            e.window.setBackgroundColor(e.fg_bg.bkg)
         end
 
-        -- call the touch callback
-        args.callback()
+        e.window.redraw()
+    end
+
+    -- initial draw
+    draw_state()
+
+    -- handle touch
+    function e.handle_touch(event)
+        -- toggle state
+        state = not state
+        draw_state()
+
+        -- call the touch callback with state
+        args.callback(state)
     end
 
     return e.get()
 end
 
-return button
+return switch_button
