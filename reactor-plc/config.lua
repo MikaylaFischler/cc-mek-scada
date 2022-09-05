4359cc3e6375867545943381bce20e140e3a321d
Mikayla Fischler <mikayla@ky8.io>
2022-09-05 16:21:59 -0400

    formatting

--- reactor-plc/config.lua
+++ reactor-plc/config.lua
@@ -3,7 +3,7 @@ local config = {}
 -- set to false to run in offline mode (safety regulation only)
 config.NETWORKED = true
 -- unique reactor ID
-config.REACTOR_ID  = 1
+config.REACTOR_ID = 1
 -- port to send packets TO server
 config.SERVER_PORT = 16000
 -- port to listen to incoming packets FROM server
