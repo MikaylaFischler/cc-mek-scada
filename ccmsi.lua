2a681d1d37db3084e79c17f8a8f44e61bad32a3e
Mikayla Fischler <mikayla@ky8.io>
2023-03-04 23:01:24 -0500

    disable debug prints and update ccmsi version for release

--- ccmsi.lua
+++ ccmsi.lua
@@ -20,7 +20,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 local function println(message) print(tostring(message)) end
 local function print(message) term.write(tostring(message)) end
 
-local CCMSI_VERSION = "v0.9m"
+local CCMSI_VERSION = "v1.0"
 
 local install_dir = "/.install-cache"
 local repo_path = "http://raw.githubusercontent.com/MikaylaFischler/cc-mek-scada/"
