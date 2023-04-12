8109a9983e8b48d85dc58af45bd85668c86c4ee1
Mikayla Fischler <mikayla@ky8.io>
2023-04-12 15:11:18 -0400

    #209 temp cleanup to pocket startup for checks to pass, actual pocket code will resolve this

--- pocket/startup.lua
+++ pocket/startup.lua
@@ -6,11 +6,11 @@ require("/initenv").init_env()
 
 local util = require("scada-common.util")
 
-local POCKET_VERSION = "alpha-v0.0.0"
+-- local POCKET_VERSION = "alpha-v0.0.0"
 
-local print = util.print
+-- local print = util.print
 local println = util.println
-local print_ts = util.print_ts
-local println_ts = util.println_ts
+-- local print_ts = util.print_ts
+-- local println_ts = util.println_ts
 
 println("Sorry, this isn't written yet :(")
