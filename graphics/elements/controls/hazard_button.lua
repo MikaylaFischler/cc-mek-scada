1edee7f64b9b3660f56a72e40c28d56416400e02
Mikayla Fischler <mikayla@ky8.io>
2023-07-09 23:42:44 -0400

    updated graphics comments

--- graphics/elements/controls/hazard_button.lua
+++ graphics/elements/controls/hazard_button.lua
@@ -14,7 +14,7 @@ local element = require("graphics.element")
 ---@field parent graphics_element
 ---@field id? string element id
 ---@field x? integer 1 if omitted
----@field y? integer 1 if omitted
+---@field y? integer auto incremented if omitted
 ---@field fg_bg? cpair foreground/background colors
 ---@field hidden? boolean true to hide on initial draw
 
