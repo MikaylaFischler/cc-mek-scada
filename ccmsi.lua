57d50e674556a7f1ea77827193b914d872803aad
Mikayla Fischler <mikayla@ky8.io>
2023-08-22 19:56:47 -0400

    #319 updated installer version

--- ccmsi.lua
+++ ccmsi.lua
@@ -18,7 +18,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 local function println(message) print(tostring(message)) end
 local function print(message) term.write(tostring(message)) end
 
-local CCMSI_VERSION = "v1.9"
+local CCMSI_VERSION = "v1.9a"
 
 local install_dir = "/.install-cache"
 local manifest_path = "https://mikaylafischler.github.io/cc-mek-scada/manifests/"
