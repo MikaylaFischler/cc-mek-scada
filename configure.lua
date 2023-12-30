622e2eeb90b46501cb59fcc5519fab53d99f85fc
Mikayla Fischler <mikayla@ky8.io>
2023-12-30 14:51:25 -0500

    more useful messages, incremented bootloader version

--- configure.lua
+++ configure.lua
@@ -7,9 +7,9 @@ elseif fs.exists("rtu/configure.lua") then
 elseif fs.exists("supervisor/configure.lua") then
     require("supervisor.configure").configure()
 elseif fs.exists("coordinator/startup.lua") then
-    print("CONFIGURE> COORDINATOR CONFIGURATOR NOT YET IMPLEMENTED IN BETA")
+    print("CONFIGURE> coordinator configurator not yet implemented (use 'edit coordinator/config.lua' to configure)")
 elseif fs.exists("pocket/startup.lua") then
-    print("CONFIGURE> POCKET CONFIGURATOR NOT YET IMPLEMENTED IN BETA")
+    print("CONFIGURE> pocket configurator not yet implemented (use 'edit pocket/config.lua' to configure)")
 else
     print("CONFIGURE> NO CONFIGURATOR FOUND")
     print("CONFIGURE> EXIT")
