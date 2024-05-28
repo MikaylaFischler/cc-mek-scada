30c921565879d6d4d042bea5bc11837148c7c275
Mikayla Fischler <mikayla@ky8.io>
2024-05-27 23:53:35 -0400

    #202 pocket reactor view

--- pocket/ui/pages/unit_page.lua
+++ pocket/ui/pages/unit_page.lua
@@ -2,6 +2,7 @@
 -- Unit Overview Page
 --
 
+local types     = require("scada-common.types")
 local util      = require("scada-common.util")
 -- local log       = require("scada-common.log")
 
@@ -17,7 +18,7 @@ local TextBox   = require("graphics.elements.textbox")
 local DataIndicator     = require("graphics.elements.indicators.data")
 local IconIndicator     = require("graphics.elements.indicators.icon")
 -- local RadIndicator      = require("graphics.elements.indicators.rad")
--- local VerticalBar       = require("graphics.elements.indicators.vbar")
+local VerticalBar       = require("graphics.elements.indicators.vbar")
 
 local PushButton        = require("graphics.elements.controls.push_button")
 
@@ -69,7 +70,7 @@ local function new_view(root)
 
     local btn_fg_bg = cpair(colors.yellow, colors.black)
     local btn_active = cpair(colors.white, colors.black)
-    -- local label = cpair(colors.lightGray, colors.black)
+    local label = cpair(colors.lightGray, colors.black)
 
     local nav_links = {}
 
@@ -81,7 +82,7 @@ local function new_view(root)
             { label = "U-" .. id, color = core.cpair(colors.black, colors.yellow), callback = function () app.switcher(id) end },
             { label = " \x13 ", color = core.cpair(colors.black, colors.red), callback = nav_links[id].alarm },
             { label = "RPS", tall = true, color = core.cpair(colors.black, colors.cyan), callback = nav_links[id].rps },
-            -- { label = " R ", color = core.cpair(colors.black, colors.lightGray), callback = function () end },
+            { label = " R ", color = core.cpair(colors.black, colors.lightGray), callback = nav_links[id].reactor  },
             { label = "RCS", tall = true, color = core.cpair(colors.black, colors.blue), callback = nav_links[id].rcs },
         }
 
@@ -154,7 +155,7 @@ local function new_view(root)
             local lu_col = cpair(colors.lightGray, colors.lightGray)
             local text_fg = cpair(colors.white, colors._INHERIT)
 
-            local rate = DataIndicator{parent=u_div,y=5,lu_colors=lu_col,label="Rate",unit="mB/t",format="%10.2f",value=0,commas=true,width=26,fg_bg=text_fg}
+            local rate = DataIndicator{parent=u_div,y=5,lu_colors=lu_col,label="Burn",unit="mB/t",format="%10.2f",value=0,commas=true,width=26,fg_bg=text_fg}
             local temp = DataIndicator{parent=u_div,lu_colors=lu_col,label="Temp",unit=db.temp_label,format="%10.2f",value=0,commas=true,width=26,fg_bg=text_fg}
 
             local ctrl = IconIndicator{parent=u_div,x=1,y=8,label="Control State",states=mode_states}
@@ -195,12 +196,11 @@ local function new_view(root)
 
             local alm_page = app.new_page(u_page, #panes)
             alm_page.tasks = { update }
-
             nav_links[i].alarm = alm_page.nav_to
 
             TextBox{parent=alm_div,y=1,text="Status Info Display",height=1,alignment=ALIGN.CENTER}
 
-            local ecam_disp = ListBox{parent=alm_div,x=2,y=3,scroll_height=500,nav_fg_bg=cpair(colors.lightGray,colors.gray),nav_active=cpair(colors.white,colors.gray)}
+            local ecam_disp = ListBox{parent=alm_div,x=2,y=3,scroll_height=100,nav_fg_bg=cpair(colors.lightGray,colors.gray),nav_active=cpair(colors.white,colors.gray)}
 
             ecam_disp.register(u_ps, "U_ECAM", function (data)
                 local ecam = textutils.unserialize(data)
@@ -239,7 +239,6 @@ local function new_view(root)
 
             local rps_page = app.new_page(u_page, #panes)
             rps_page.tasks = { update }
-
             nav_links[i].rps = rps_page.nav_to
 
             TextBox{parent=rps_div,y=1,text="Protection System",height=1,alignment=ALIGN.CENTER}
@@ -273,6 +272,130 @@ local function new_view(root)
             rps_exw.register(u_ps, "ex_waste", rps_exw.update)
             rps_loc.register(u_ps, "low_cool", rps_loc.update)
             rps_exh.register(u_ps, "ex_hcool", rps_exh.update)
+
+            --#endregion
+
+            --#region Reactor Tab
+
+            local rct_pane = Div{parent=page_div}
+            local rct_div = Div{parent=rct_pane,x=2,width=main.get_width()-2}
+            table.insert(panes, rct_div)
+
+            local rct_page = app.new_page(u_page, #panes)
+            rct_page.tasks = { update }
+            nav_links[i].reactor = rct_page.nav_to
+
+            TextBox{parent=rct_div,y=1,text="Fission Reactor",height=1,alignment=ALIGN.CENTER}
+
+            local fuel  = VerticalBar{parent=rct_div,x=1,y=4,fg_bg=cpair(colors.lightGray,colors.gray),height=5,width=1}
+            local ccool = VerticalBar{parent=rct_div,x=3,y=4,fg_bg=cpair(colors.blue,colors.gray),height=5,width=1}
+            local hcool = VerticalBar{parent=rct_div,x=19,y=4,fg_bg=cpair(colors.white,colors.gray),height=5,width=1}
+            local waste = VerticalBar{parent=rct_div,x=21,y=4,fg_bg=cpair(colors.brown,colors.gray),height=5,width=1}
+
+            TextBox{parent=rct_div,text="F",x=1,y=3,width=1,height=1,fg_bg=label}
+            TextBox{parent=rct_div,text="C",x=3,y=3,width=1,height=1,fg_bg=label}
+            TextBox{parent=rct_div,text="H",x=19,y=3,width=1,height=1,fg_bg=label}
+            TextBox{parent=rct_div,text="W",x=21,y=3,width=1,height=1,fg_bg=label}
+
+            fuel.register(u_ps, "fuel_fill", fuel.update)
+            ccool.register(u_ps, "ccool_fill", ccool.update)
+            hcool.register(u_ps, "hcool_fill", hcool.update)
+            waste.register(u_ps, "waste_fill", waste.update)
+
+            ccool.register(u_ps, "ccool_type", function (type)
+                if type == types.FLUID.SODIUM then
+                    ccool.recolor(cpair(colors.lightBlue, colors.gray))
+                else
+                    ccool.recolor(cpair(colors.blue, colors.gray))
+                end
+            end)
+
+            hcool.register(u_ps, "hcool_type", function (type)
+                if type == types.FLUID.SUPERHEATED_SODIUM then
+                    hcool.recolor(cpair(colors.orange, colors.gray))
+                else
+                    hcool.recolor(cpair(colors.white, colors.gray))
+                end
+            end)
+
+            TextBox{parent=rct_div,text="Burn Rate",x=5,y=5,width=13,height=1,fg_bg=label}
+            local burn_rate = DataIndicator{parent=rct_div,x=5,y=6,lu_colors=lu_col,label="",unit="mB/t",format="%8.2f",value=1024.99,commas=true,width=13,fg_bg=text_fg}
+            TextBox{parent=rct_div,text="Temperature",x=5,y=7,width=13,height=1,fg_bg=label}
+            local t_prec = util.trinary(db.temp_label == types.TEMP_SCALE_UNITS[types.TEMP_SCALE.KELVIN], 11, 10)
+            local core_temp = DataIndicator{parent=rct_div,x=5,y=8,lu_colors=lu_col,label="",unit=db.temp_label,format="%"..t_prec..".2f",value=17802.03,commas=true,width=13,fg_bg=text_fg}
+
+            local r_state = IconIndicator{parent=rct_div,x=7,y=3,label="State",states=mode_states}
+
+            burn_rate.register(u_ps, "act_burn_rate", burn_rate.update)
+            core_temp.register(u_ps, "temp", function (t) core_temp.update(db.temp_convert(t)) end)
+            r_state.register(u_ps, "U_ControlStatus", r_state.update)
+
+            local r_temp = IconIndicator{parent=rct_div,y=10,label="Reactor Temp. Hi",states=red_ind_s}
+            local r_rhdt = IconIndicator{parent=rct_div,label="Hi Delta Temp.",states=yel_ind_s}
+            local r_firl = IconIndicator{parent=rct_div,label="Fuel Rate Lo",states=yel_ind_s}
+            local r_wloc = IconIndicator{parent=rct_div,label="Waste Line Occl.",states=yel_ind_s}
+            local r_hsrt = IconIndicator{parent=rct_div,label="Hi Startup Rate",states=yel_ind_s}
+
+            r_temp.register(u_ps, "ReactorTempHigh", r_temp.update)
+            r_rhdt.register(u_ps, "ReactorHighDeltaT", r_rhdt.update)
+            r_firl.register(u_ps, "FuelInputRateLow", r_firl.update)
+            r_wloc.register(u_ps, "WasteLineOcclusion", r_wloc.update)
+            r_hsrt.register(u_ps, "HighStartupRate", r_hsrt.update)
+
+            TextBox{parent=rct_div,text="HR",x=1,y=16,width=4,height=1,fg_bg=label}
+            local heating_r = DataIndicator{parent=rct_div,x=6,y=16,lu_colors=lu_col,label="",unit="mB/t",format="%11.0f",value=0,commas=true,width=16,fg_bg=text_fg}
+            TextBox{parent=rct_div,text="DMG",x=1,y=17,width=4,height=1,fg_bg=label}
+            local damage_p = DataIndicator{parent=rct_div,x=6,y=17,lu_colors=lu_col,label="",unit="%",format="%11.2f",value=0,width=16,fg_bg=text_fg}
+
+            heating_r.register(u_ps, "heating_rate", heating_r.update)
+            damage_p.register(u_ps, "damage", damage_p.update)
+
+            local rct_ext_div = Div{parent=rct_pane,x=2,width=main.get_width()-2}
+            table.insert(panes, rct_ext_div)
+
+            local rct_ext_page = app.new_page(rct_page, #panes)
+            rct_ext_page.tasks = { update }
+
+            PushButton{parent=rct_div,x=9,y=18,text="MORE",min_width=6,fg_bg=cpair(colors.lightGray,colors.gray),active_fg_bg=cpair(colors.gray,colors.lightGray),callback=rct_ext_page.nav_to}
+            PushButton{parent=rct_ext_div,x=9,y=18,text="BACK",min_width=6,fg_bg=cpair(colors.lightGray,colors.gray),active_fg_bg=cpair(colors.gray,colors.lightGray),callback=rct_page.nav_to}
+
+            TextBox{parent=rct_ext_div,y=1,text="More Reactor Info",height=1,alignment=ALIGN.CENTER}
+
+            TextBox{parent=rct_ext_div,text="Fuel Tank",x=1,y=3,width=9,height=1,fg_bg=label}
+            local fuel_p = DataIndicator{parent=rct_ext_div,x=14,y=3,lu_colors=lu_col,label="",unit="%",format="%6.2f",value=0,width=8,fg_bg=text_fg}
+            local fuel_amnt = DataIndicator{parent=rct_ext_div,x=1,y=4,lu_colors=lu_col,label="",unit="mB/t",format="%16.0f",value=0,commas=true,width=21,fg_bg=text_fg}
+
+            fuel_p.register(u_ps, "fuel_fill", function (x) fuel_p.update(x * 100) end)
+            fuel_amnt.register(u_ps, "fuel", fuel_amnt.update)
+
+            TextBox{parent=rct_ext_div,text="Cool Coolant",x=1,y=6,width=12,height=1,fg_bg=label}
+            local cooled_p = DataIndicator{parent=rct_ext_div,x=14,y=6,lu_colors=lu_col,label="",unit="%",format="%6.2f",value=0,width=8,fg_bg=text_fg}
+            local ccool_amnt = DataIndicator{parent=rct_ext_div,x=1,y=7,lu_colors=lu_col,label="",unit="mB/t",format="%16.0f",value=0,commas=true,width=21,fg_bg=text_fg}
+
+            cooled_p.register(u_ps, "ccool_fill", function (x) cooled_p.update(x * 100) end)
+            ccool_amnt.register(u_ps, "ccool_amnt", ccool_amnt.update)
+
+            TextBox{parent=rct_ext_div,text="Hot Coolant",x=1,y=9,width=12,height=1,fg_bg=label}
+            local heated_p = DataIndicator{parent=rct_ext_div,x=14,y=9,lu_colors=lu_col,label="",unit="%",format="%6.2f",value=0,width=8,fg_bg=text_fg}
+            local hcool_amnt = DataIndicator{parent=rct_ext_div,x=1,y=10,lu_colors=lu_col,label="",unit="mB/t",format="%16.0f",value=0,commas=true,width=21,fg_bg=text_fg}
+
+            heated_p.register(u_ps, "hcool_fill", function (x) heated_p.update(x * 100) end)
+            hcool_amnt.register(u_ps, "hcool_amnt", hcool_amnt.update)
+
+            TextBox{parent=rct_ext_div,text="Waste Tank",x=1,y=12,width=10,height=1,fg_bg=label}
+            local waste_p = DataIndicator{parent=rct_ext_div,x=14,y=12,lu_colors=lu_col,label="",unit="%",format="%6.2f",value=0,width=8,fg_bg=text_fg}
+            local waste_amnt = DataIndicator{parent=rct_ext_div,x=1,y=13,lu_colors=lu_col,label="",unit="mB/t",format="%16.0f",value=0,commas=true,width=21,fg_bg=text_fg}
+
+            waste_p.register(u_ps, "waste_fill", function (x) waste_p.update(x * 100) end)
+            waste_amnt.register(u_ps, "waste", waste_amnt.update)
+
+            TextBox{parent=rct_ext_div,text="Boil Eff.",x=1,y=15,width=9,height=1,fg_bg=label}
+            TextBox{parent=rct_ext_div,text="Env. Loss",x=1,y=16,width=9,height=1,fg_bg=label}
+            local boil_eff = DataIndicator{parent=rct_ext_div,x=11,y=15,lu_colors=lu_col,label="",unit="%",format="%9.2f",value=0,width=11,fg_bg=text_fg}
+            local env_loss = DataIndicator{parent=rct_ext_div,x=11,y=16,lu_colors=lu_col,label="",unit="",format="%11.8f",value=0,width=11,fg_bg=text_fg}
+
+            boil_eff.register(u_ps, "boil_eff", function (x) boil_eff.update(x * 100) end)
+            env_loss.register(u_ps, "env_loss", env_loss.update)
 
             --#endregion
 
