ea1bcbf81c0efdd0e8571d9facaf458f71cc6dad
Mikayla Fischler <mikayla@ky8.io>
2024-06-29 23:52:34 -0400

    luacheck fix 2

--- build/_offline.lua
+++ build/_offline.lua
@@ -1,5 +1,5 @@
 ---@diagnostic disable: undefined-global
--- luacheck: globals install_manifest, ccmsi_offline, app_files, dep_files, lgray, green, white
+-- luacheck: push ignore install_manifest ccmsi_offline app_files dep_files lgray green white
 
 local b64_lookup = {
     ['A'] = 0,  ['B'] = 1,  ['C'] = 2,  ['D'] = 3,  ['E'] = 4,  ['F'] = 5,  ['G'] = 6,  ['H'] = 7,  ['I'] = 8,  ['J'] = 9,  ['K'] = 10, ['L'] = 11, ['M'] = 12, ['N'] = 13, ['O'] = 14, ['P'] = 15, ['Q'] = 16, ['R'] = 17, ['S'] = 18, ['T'] = 19, ['U'] = 20, ['V'] = 21, ['W'] = 22, ['X'] = 23, ['Y'] = 24, ['Z'] = 25,
