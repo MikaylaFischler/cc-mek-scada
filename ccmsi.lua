e81b0b38404b491e50d2db34ca70dfecbcf10775
Mikayla Fischler <mikayla@ky8.io>
2026-07-11 14:42:11 -0400

    fixed installer not checking local versions on reinstall operations

--- ccmsi.lua
+++ ccmsi.lua
@@ -15,7 +15,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ]]--
 
-local CCMSI_VERSION = "v1.22"
+local CCMSI_VERSION = "v1.23"
 
 local IS_PKT = pocket ~= nil -- luacheck: ignore pocket
 
@@ -385,22 +385,20 @@ elseif mode == "install" or mode == "update" then
 
 	-- try to load local versions
 	ok, l_manifest = read_local_manifest()
-	if mode == "update" and not update_installer then
-		if not ok then
-			red();pln("Failed to load local installation information, cannot update.");white()
-			return
-		else
-			ver.boot.v_local = l_manifest.versions.bootloader
-			ver.app.v_local = l_manifest.versions[app]
-			ver.comms.v_local = l_manifest.versions.comms
-			ver.common.v_local = l_manifest.versions.common
-			ver.graphics.v_local = l_manifest.versions.graphics
-			ver.lockbox.v_local = l_manifest.versions.lockbox
+	if (mode == "update" and not update_installer) and not ok then
+		red();pln("Failed to load local installation information, cannot update.");white()
+		return
+	else
+		ver.boot.v_local = l_manifest.versions.bootloader
+		ver.app.v_local = l_manifest.versions[app]
+		ver.comms.v_local = l_manifest.versions.comms
+		ver.common.v_local = l_manifest.versions.common
+		ver.graphics.v_local = l_manifest.versions.graphics
+		ver.lockbox.v_local = l_manifest.versions.lockbox
 
-			if l_manifest.versions[app] == nil then
-				red();pln("Another application is already installed, please uninstall it before installing a new application.");white()
-				return
-			end
+		if l_manifest.versions[app] == nil then
+			red();pln("Another application is already installed, please uninstall it before installing a new application.");white()
+			return
 		end
 	end
 
