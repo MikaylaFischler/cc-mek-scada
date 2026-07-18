a2bfafb63e6e85266e1928827a6374925454899b
Mikayla Fischler <mikayla@ky8.io>
2026-07-18 16:50:49 -0400

    #729 fix for ETA with energy core

--- supervisor/facility.lua
+++ supervisor/facility.lua
@@ -797,7 +797,7 @@ function facility.new(config)
 
             if db.build.last_update > 0 then
                 local fe_per_ms = self.avg_net.get()
-                local remaining = util.joules_to_fe_rf(util.trinary(fe_per_ms >= 0, db.build.max_energy - db.state.energy, db.state.energy))
+                local remaining = util.trinary(fe_per_ms >= 0, db.build.max_energy - db.state.energy, db.state.energy)
                 status.power[4] = remaining / fe_per_ms
             end
         end
